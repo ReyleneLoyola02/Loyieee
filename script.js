bc6753f0740228aba76ef3dcacc6d5852fd31a41
ReyleneLoyola02 <124236574+ReyleneLoyola02@users.noreply.github.com>
2023-05-12 12:48:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let bluetoothDevice;
-let buzzerCharacteristic;
+let serialCharacteristic;
 
 const connectButton = document.getElementById('connectButton');
 const disconnectButton = document.getElementById('disconnectButton');
@@ -14,12 +14,12 @@ deactivateButton.addEventListener('click', deactivateBuzzer);
 async function connectToDevice() {
   try {
     bluetoothDevice = await navigator.bluetooth.requestDevice({
-      filters: [{ services: ['serial_port'] }]
+      filters: [{ services: ['00001101-0000-1000-8000-00805F9B34FB'] }]
     });
-  
+
     const server = await bluetoothDevice.gatt.connect();
-    const service = await server.getPrimaryService('serial_port');
-    buzzerCharacteristic = await service.getCharacteristic('serial_port');
+    const service = await server.getPrimaryService('00001101-0000-1000-8000-00805F9B34FB');
+    serialCharacteristic = await service.getCharacteristic('00001101-0000-1000-8000-00805F9B34FB');
 
     console.log('Connected to Bluetooth device:', bluetoothDevice);
   } catch (error) {
@@ -35,18 +35,18 @@ function disconnectFromDevice() {
 }
 
 function activateBuzzer() {
-  if (buzzerCharacteristic && buzzerCharacteristic.writeValue) {
+  if (serialCharacteristic && serialCharacteristic.writeValue) {
     const data = new Uint8Array([1]); // Send '1' to activate buzzer
-    buzzerCharacteristic.writeValue(data)
+    serialCharacteristic.writeValue(data)
       .then(() => console.log('Buzzer activated'))
       .catch(error => console.error('Error activating buzzer:', error));
   }
 }
 
 function deactivateBuzzer() {
-  if (buzzerCharacteristic && buzzerCharacteristic.writeValue) {
+  if (serialCharacteristic && serialCharacteristic.writeValue) {
     const data = new Uint8Array([0]); // Send '0' to deactivate buzzer
-    buzzerCharacteristic.writeValue(data)
+    serialCharacteristic.writeValue(data)
       .then(() => console.log('Buzzer deactivated'))
       .catch(error => console.error('Error deactivating buzzer:', error));
   }
